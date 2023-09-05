256da035c68e61e9b3cb55aa2df7336e294816da
Wyatt Stephenson <wyattstephenson@rocketmail.com>
2023-09-05 14:03:49 -0600

    working nav bar on home page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -7,6 +7,10 @@ function HomePage () {
     return(
 <>
 <NavBar />
+<div>
+    <h1>Home Page</h1>
+<span>Welcome to my online store, where all of your product related hopes and dreams will come true!</span>
+</div>
 </>
     )
 }
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,20 +1,23 @@
 import react from "react"
+import HomePage from "./HomePage"
+import CartPage from "./CartPage"
+import Store from "./Store"
 import {BrowserRouter, Router, Routes, Link} from "react-router-dom"
 
 function NavBar () {
     return (
         <>
     <span>
-        
+        <Link to="/">Home Page</Link>
+    </span>
+    <span>   |    
+        <Link to="/Store">Store</Link>
+    </span>
+    <span>   |    
+        <Link to="/CartPage">Cart Page</Link>
     </span>
     <span>
-
-    </span>
-    <span>
-
-    </span>
-    <span>
-
+        {/* <Link to="" component={}></Link> */}
     </span>
     </>
     )
