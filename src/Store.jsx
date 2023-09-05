fc3c7e7a0faef86dcef2c1fc051baa03e36c7262
Wyatt Stephenson <wyattstephenson@rocketmail.com>
2023-09-05 15:16:54 -0600

    working fetch

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,23 +1,24 @@
-import react from "react"
+import react, { useContext } from "react"
 import HomePage from "./HomePage"
 import CartPage from "./CartPage"
 import NavBar from "./NavBar"
 import {useEffect} from "react"
+import {useState} from "react"
 import {BrowserRouter, Router, Routes, Link} from "react-router-dom"
 
 function Store () {
+
+    const [items, setItems] = useState([])
 
     useEffect(()=> {
         fetch(`https://fakestoreapi.com/products`)
         .then(r => r.json())
         .then((r) => {
-
-
-            
+            setItems(r)
         })
-    })
+    }, [])
 
-
+    console.log(items)
 
 return (
     <>
