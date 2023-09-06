e2548cfe25ede00dc303045bf427c8f7421cfa5f
Wyatt Stephenson <wyattstephenson@rocketmail.com>
2023-09-06 12:35:52 -0600

    Functional NavBar, Store, and HomePage

--- src/Store.jsx
+++ src/Store.jsx
@@ -6,6 +6,11 @@ import {useEffect} from "react"
 import {useState} from "react"
 import {BrowserRouter, Router, Routes, Link} from "react-router-dom"
 
+const style5 = {
+    width: "20%",
+    // height: "20%"
+}
+
 function Store () {
 
     const [items, setItems] = useState([])
@@ -18,25 +23,30 @@ function Store () {
         })
     }, [])
 
-    // console.log(items)
+    console.log(items)
 
     function renderProductCards () {
             return items.map((item)=>{
-            return(<>
-            <div>{item.title}</div><br></br>
-            <div>{item.description}</div><br></br>
-            <div>${item.price}</div><br></br>
-            </>)
+            return(
+              
+                <div key={item.id} >
+                <img style={style5} src={item.image} alt={item.title} />
+                <p>{item.title}</p>
+                <p>{item.description}</p>
+                <p>${item.price}</p>
+                <span>Add to cart!</span>
+              </div>
+               
+            ) 
         })
-        
     }
     
-
 return (
     <>
     <div>
     <NavBar />
     Welcome to the Store Page!
+    <br></br>
     <br></br>
     {renderProductCards()}
     {}
