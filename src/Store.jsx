e0c4c1fad2af4a882fafdd193ad2d1e7027e5744
Wyatt Stephenson <wyattstephenson@rocketmail.com>
2023-09-07 13:59:19 -0600

    working cart, quantity not working

--- src/Store.jsx
+++ src/Store.jsx
@@ -13,6 +13,8 @@ const style5 = {
 function Store () {
     const {cartItems, setCartItems} = useContext(shoppingCart)
     let tempCart = []
+    // const [tempCart2, setTempCart2] = useState()
+
     let quantityArray = [0]
 
     //Used to render the items that can be purchased, difine by the API.
@@ -35,7 +37,6 @@ function Store () {
             tempCart.push(item)
         }
         const currentIndex = (tempCart.findIndex((cartItem) => cartItem.id === item.id)) 
-        console.log("current Index is :" + currentIndex)
 
         //if it is a new item being added to the cart, add a new item to the array.
         if(quantityArray[currentIndex] == undefined){
@@ -45,14 +46,25 @@ function Store () {
         //add item quantity to cart
         quantityArray[currentIndex] += 1 
 
+        console.log("current Index is :" + currentIndex)
         console.log("item is :" + tempCart[currentIndex].title)
         console.log("number of items :" + quantityArray[currentIndex])
 
+      
+        let updatedCart = [...cartItems, ...tempCart]
+        setCartItems(updatedCart)
     }
 
-   console.log(quantityArray)
+
 
     function renderProductCards () {
+        useEffect (()=>{           
+            setCartItems(tempCart)
+       }, []
+       )
+
+    //    console.log("cart items :" + cartItems)
+
             return items.map((item)=>{
             return(
               
